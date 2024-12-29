1e72ef33e7e14b458d58987fc63779a4ea09bd37
alexanderProgramer <cursoexcelhs@gmail.com>
2024-12-29 18:15:38 -0500

    make carrusell

--- scripts/changeImageHover.js
+++ scripts/changeImageHover.js
@@ -1,9 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const images = document.querySelectorAll('img[data-hover-src]');
+    const images = [
+        { src: 'img/wide.jpg', hoverSrc: './img/wide2.png' },
+        { src: 'img/reisuit.jpg', hoverSrc: './img/reissued.png' },
+       
+
+    ];
+
+    let currentIndex = 0;
+    const carouselImage = document.getElementById('carouselImage');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+
+    const updateImage = () => {
+        console.log("current index" , currentIndex)
+        carouselImage.src = images[currentIndex].src;
+        carouselImage.setAttribute('data-hover-src', images[currentIndex].hoverSrc);
+        console.log("hover src is ", images[currentIndex].hoverSrc);
+    };
 
     const addHoverEffect = (img) => {
         const originalSrc = img.src;
         const hoverSrc = img.getAttribute('data-hover-src');
+        console.log("addhoverer efect is :",hoverSrc);
 
         const onMouseOver = () => img.src = hoverSrc;
         const onMouseOut = () => img.src = originalSrc;
@@ -12,5 +30,18 @@ document.addEventListener('DOMContentLoaded', function() {
         img.addEventListener('mouseout', onMouseOut);
     };
 
-    images.forEach(img => addHoverEffect(img));
+    prevBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+        updateImage();
+        addHoverEffect(carouselImage);
+    });
+
+    nextBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+        updateImage();
+        addHoverEffect(carouselImage);//important for makeeffect
+    });
+
+    addHoverEffect(carouselImage);
+    updateImage();
 });
